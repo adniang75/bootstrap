2a8557cd8f744a36ffd9990373e3f63aae9bcf0d
Alassane NIANG <adniang75@gmail.com>
2022-09-01 13:39:15 +0200

    Reactor - Operators MapTest

---
+++ src/test/java/rsb/reactor/MapTest.java
@@ -0,0 +1,20 @@
+package rsb.reactor;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+class MapTest {
+
+    @Test
+    void maps() {
+        var data = Flux
+                .just("a", "b", "c")
+                .map(String::toUpperCase);
+        StepVerifier
+                .create(data)
+                .expectNext("A", "B", "C")
+                .verifyComplete();
+    }
+
+}
