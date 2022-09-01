06067e0eb2a115f13e39a10b67535cd838d46686
Alassane NIANG <adniang75@gmail.com>
2022-09-01 17:16:06 +0200

    Reactor - Operators HandleTest

---
+++ src/test/java/rsb/reactor/HandleTest.java
@@ -0,0 +1,47 @@
+package rsb.reactor;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Log4j2
+class HandleTest {
+
+    @Test
+    @DisplayName("Handle")
+    void handle() {
+        StepVerifier
+                .create(this.handle(5, 4))
+                .expectNext(0, 1, 2, 3)
+                .expectError(IllegalArgumentException.class)
+                .verify();
+        StepVerifier
+                .create(this.handle(3, 3))
+                .expectNext(0, 1, 2)
+                .verifyComplete();
+    }
+
+    private Flux<Integer> handle(int max, int numberToError) {
+        return Flux
+                .range(0, max)
+                .handle((value, sink) -> {
+                    var upTo = Stream.iterate(0, i -> i < numberToError, i -> i + 1)
+                            .collect(Collectors.toList());
+                    if (upTo.contains(value)) {
+                        sink.next(value);
+                        return;
+                    }
+                    if (value == numberToError) {
+                        sink.error(new IllegalArgumentException("No 4 for you!"));
+                        return;
+                    }
+                    sink.complete();
+                });
+    }
+
+}
